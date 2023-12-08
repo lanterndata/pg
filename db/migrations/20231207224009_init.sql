511fe3e6caac6c9ed51702826bd05052c22bb8c0
Di Qi <di@racer.app>
2023-12-07 18:36:09 -0800

    Add additional scraping functions and constants

--- db/migrations/20231207224009_init.sql
+++ db/migrations/20231207224009_init.sql
@@ -1,32 +1,24 @@
 -- migrate:up
 
-create table lists (
-    name text primary key,
-    description text,
-    category text
+create table scrape_logs (
+  list text,
+  date text,
+  completed_at timestamptz not null default now(),
+  primary key (list, date)
 );
 
 create table messages (
-    id text primary key,
-    list_name text references lists(name),
-    in_reply_to text references messages(id),
-    ts timestamptz,
-    subject text,
-    from_email text,
-    from_name text
+  id text primary key,
+  lists text[],
+  in_reply_to text references messages(id),
+  ts timestamptz,
+  subject text,
+  from text,
+  to text[],
+  cc text[]
 );
-
-create table message_recipients (
-    message_id text references messages(id),
-    type text, -- to, cc, bcc
-    email text,
-    name text,
-    primary key (message_id, type, email)
-);
-create index idx_message_recipients on message_recipients (message_id);
 
 -- migrate:down
 
-drop table lists;
+drop table scrape_logs;
 drop table messages;
-drop table message_recipients;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,72 @@
-'use client';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
-export default function Home() {
+// Sample URL: https://www.postgresql.org/list/pgsql-admin/2023-12/
+async function fetchMessageUrls(list: string, date: string) {
+  const url = `https://www.postgresql.org/list/${list}/${date}/`;
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+
+  const messageLinks = $('a[href^="/message-id/"]');
+  const messageIds = messageLinks
+    .map((index, element) => $(element).attr('href'))
+    .get()
+    .map((messageUrls) => messageUrls.split('/').pop()!);
+
+  return messageIds;
+}
+
+// Sample URL: https://www.postgresql.org/message-id/34a83d6f68c2d513a88acb40cdc581c43586a746.camel%40cybertec.at
+async function fetchMessage(id: string) {
+  const url = `https://www.postgresql.org/message-id/${id}`;
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+
+  const lists = $('.listname a')
+    .map((index, element) => $(element).text())
+    .get();
+
+  const to = $('tr th:contains("To:") + td')
+    .text()
+    .split(',')
+    .map((address) => address.trim());
+
+  const subject = $('tr th:contains("Subject:") + td').text().trim();
+
+  const ts = $('tr th:contains("Date:") + td').text().trim();
+
+  const from = $('tr th:contains("From:") + td').text().trim();
+
+  const cc = $('tr th:contains("Cc:") + td')
+    .text()
+    .split(',')
+    .map((email) => email.trim())
+    .filter((email) => !!email);
+
+  const inResponseToHeader = $('h3.messages:contains("In response to")');
+  const messageResponses = inResponseToHeader.next('.message-responses');
+  const messageIds = messageResponses
+    .find('a')
+    .map((index, element) => $(element).attr('href')!.split('/').pop())
+    .get();
+  const inReplyTo = messageIds[0];
+
+  return {
+    id,
+    lists,
+    to,
+    subject,
+    ts,
+    from,
+    cc,
+    inReplyTo,
+  };
+}
+
+async function scrapeMessages() {
+  return;
+}
+
+export default async function Home() {
   return <main></main>;
 }
