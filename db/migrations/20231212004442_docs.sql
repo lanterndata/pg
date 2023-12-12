e56a775a9e88daf4a9bd31b0fbbc6c5bee94ab7e
Di Qi <di@racer.app>
2023-12-11 21:12:16 -0800

    Initial add docs

---
+++ db/migrations/20231212004442_docs.sql
@@ -0,0 +1,31 @@
+-- migrate:up
+
+alter table scrape_logs rename to messages_scrape_logs;
+alter table error_messages rename to messages_scrape_errors;
+
+create table docs (
+  id serial primary key,
+  branch text not null,
+  path text not null,
+  body text not null,
+  body_tsvector tsvector GENERATED ALWAYS AS (to_tsvector('english', coalesce(body, ''))) STORED,
+);
+
+create unique index docs_path_branch_idx on docs (path, branch);
+create index docs_body_textsearch_idx on docs using GIN (body_tsvector);
+
+create table docs_chunks (
+  id serial primary key,
+  doc_id integer not null references docs(id),
+  ordinality integer not null,
+  chunk_content text not null
+);
+create index docs_chunks_doc_id_idx on docs_chunks (doc_id);
+
+-- migrate:down
+
+drop table docs_chunks;
+drop table docs;
+
+alter table messages_scrape_logs rename to scrape_logs;
+alter table messages_scrape_errors rename to error_messages;
