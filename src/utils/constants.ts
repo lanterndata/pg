c87bad7a0832aac512ae10be2b368c318a331dab
Di Qi <di@lantern.dev>
2024-09-17 17:57:32 -0700

    Add new mailing lists

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,9 +3,32 @@ export const CATEGORY_LISTS = [
     category: 'user',
     lists: [
       {
+        list: 'pgsql-admin',
+        description:
+          'PostgreSQL administration (excludes pgAdmin application support, which can be found on the pgadmin-support list)',
+      },
+      {
+        list: 'pgsql-announce',
+        description:
+          'Announcement list pertaining to PostgreSQL and various third party software.',
+      },
+      {
+        list: 'pgsql-bugs',
+        description: 'If you find a bug, please use the bug reporting form.',
+      },
+      {
+        list: 'pgsql-docs',
+        description: 'Discussion regarding PostgreSQL documentation.',
+      },
+      {
         list: 'pgsql-general',
         description:
           "General discussion area for users. Apart from compile, acceptance test, and bug problems, most new users will probably only be interested in this mailing list (unless they want to contribute to development or documentation). All non-bug related questions regarding PostgreSQL's version of SQL, and all installation related questions that do not involve bugs or failed compiles, should be restricted to this area. Please note that many of the developers monitor this area.",
+      },
+      {
+        list: 'pgsql-hackers',
+        description:
+          'The PostgreSQL developers team lives here. Discussion of current development issues, problems and bugs, and proposed new features. If your question cannot be answered by people in the other lists, and it is likely that only a developer will know the answer, you may re-post your question in this list. You must try elsewhere first!',
       },
       {
         list: 'pgsql-interfaces',
@@ -24,6 +47,11 @@ export const CATEGORY_LISTS = [
       {
         list: 'pgsql-sql',
         description: 'Discussion for users on SQL related matters.',
+      },
+      {
+        list: 'pgsql-testers',
+        description:
+          'For active testers of PostgreSQL Alphas and Betas to report test results.',
       },
     ],
   },
