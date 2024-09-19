6d28bb0e0aa52eeaf6abe2f9e0447d1cdc33d493
Di Qi <di@lantern.dev>
2024-09-19 12:54:20 -0700

    Set metadata base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const font = Source_Sans_3({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Lantern | Search Postgres Mailing Lists',
   description: 'Postgres mailing list search engine, powered by Lantern',
+  metadataBase: new URL('https://pg.lantern.dev'),
 };
 
 export default function RootLayout({
